549b63375f3a600d58cb328653ef88e204b20382
Sarah Skry <sarah.skrydstrup@gmail.com>
2024-06-06 14:07:02 -0400

    updated seed data for movielists to include watchlist for user 1

--- server/seeds/01_movielists.js
+++ server/seeds/01_movielists.js
@@ -9,19 +9,26 @@ exports.seed = async function(knex) {
     {
       id: 1,
       user_id: 1,
+      list_title: "watched",
+      created_at: "2024-05-04 2:34:56",
+      updated_at: null,
+    },
+    {
+      id: 2,
+      user_id: 1,
       list_title: "Sidney Poitier",
       created_at: "2024-05-05 12:34:56",
       updated_at: null,
     },
     {
-      id: 2, 
+      id: 3, 
       user_id: 1,
       list_title: "classics",
       created_at: "2024-05-10 1:34:56",
       updated_at: null,
     },
     {
-      id: 3, 
+      id: 4, 
       user_id: 1,
       list_title: "Highschool Favs",
       created_at: "2024-05-12 1:34:56",
