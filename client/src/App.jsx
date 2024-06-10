017f12c53e46daad96b07a9754bace791e4f8e54
Sarah Skry <sarah.skrydstrup@gmail.com>
2024-06-10 13:01:06 -0400

    updates to header and dashboard styles, and got carried away with route to Lists page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import Login from "./pages/Login/Login";
 import UserDashboard from "./pages/Dashboard/Dashboard";
 import Splash from "./pages/Splash/Splash";
+import Lists from "./pages/Lists/Lists";
 
 function App() {
   // token
@@ -17,6 +18,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login setToken={setToken} />} />
         <Route path="/dashboard" element={<UserDashboard token={token}/>} />
+        <Route path="/lists" element={<Lists token={token} />} />
       </Routes>
     </BrowserRouter>
    
--- server/routes/moviesRouter.js
+++ server/routes/moviesRouter.js
@@ -3,16 +3,23 @@ const router = express.Router();
 const { addMovieToWatchedList } = require('../controllers/moviescontroller');
 const { authorize } = require('../middleware/authorize');
 
-// router.post('/watched', authorize, addMovieToWatchedList);
-
-// // module.exports = router;
-
-
-
-// module.exports = router;
-
+// post movie from seach to watched list
 router.post('/', authorize, (req, res) => {
     addMovieToWatchedList(req, res);
+});
+
+//get movies in a list
+router.get('watched/list/:listId', async (req, res) => {
+    const { listId } = req.params;
+    try {
+        const movies = await knex('ratings')
+            .join('movielists', 'ratings.list_id', 'movielists.id')
+            .select('ratings.movie_id')
+            .where('movielists.id', listId);
+        res.json(movies);
+    } catch (err) {
+        res.status(500).json({ err: "Failed to fetch movies" });
+    }
 });
 
 module.exports = router;
